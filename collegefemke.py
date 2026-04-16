f9965da35fd974ed2ab82da3cf90dba51d24b02c
femkeboersma <femke.boersma@student.uva.nl>
2026-04-16 13:17:05 +0200

    opdonderen

--- collegefemke.py
+++ collegefemke.py
@@ -1,2 +1,3 @@
 print("Hoi Walid wil je de volgende keer niet komen?")
 print("Dan zijn er geen ongemakkelijke handshakes meer")
+print("De oorlog is een stuk beter, dan praat die hond Wally tenminste niet tegen mij")
