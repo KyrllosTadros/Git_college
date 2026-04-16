96a95aec24280b71f4641aaa9b8ee488176e9efb
femkeboersma <femke.boersma@student.uva.nl>
2026-04-16 13:28:17 +0200

    veel plezier

--- collegewalid.py
+++ collegewalid.py
@@ -1,3 +1,4 @@
 print ("Hoi Femke")
 print("Hoi Walid")
 print("Deze daggoe gaat niet meer tegen je praten, jij bent zo'n bot die hij laat liggen FE|<E")
+print("Goed dat je zelf inziet dat je een daggoe bent Wally. Je hoort op de straat in je trainingspak voor daggoes WALLY.")
